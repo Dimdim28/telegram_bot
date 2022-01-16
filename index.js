eb747fa4cf0d5040ebe5c2851ce7b090a3023b38
Михайло Креславський <89911844+kreslavskiy@users.noreply.github.com>
2022-01-16 17:58:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,139 +1,96 @@
-"use strict";
+'use strict';
 
-//const { messageTypes } = require('node-telegram-bot-api/src/telegram');
-const { Telegraf } = require("telegraf");
-const fs = require("fs");
+const { Telegraf } = require('telegraf');
+const fs = require('fs');
 
-const GifPath = "gif.txt";
-const StickPath = "sticker.txt";
-const MessPath = "messagearray.txt";
+const token = '5041846136:AAFjwTdTJI9vwbYiLLvWDe4OupltRqesXrc';
+const bot = new Telegraf(token);
 
-const add = (data, file) => {
-  fs.appendFileSync(file, `${data}`, () => {});
-  fs.appendFileSync(file, "\n", () => {});
-};
+const GifPath = 'gif.txt';
+const StickPath = 'sticker.txt';
+const MessPath = 'messagearray.txt';
 
-const contentUTF8 = (file) => fs.readFileSync(file, "utf8");
+const contentUTF8 = (file) => fs.readFileSync(file, 'utf8');
+
 const GifContent = contentUTF8(GifPath);
 const StickContent = contentUTF8(StickPath);
 const MessContent = contentUTF8(MessPath);
 
-const AddToArray = (data) => data.split("\n");
+const AddToArray = (data) => data.split('\n');
 
 const randomMessageFromArray = (array) => {
-  const index = Math.floor(Math.random() * (array.length - 1));
+  const index = Math.round(Math.random() * (array.length - 1));
   const message = array[index];
   return message;
 };
 
-const token = "5041846136:AAFjwTdTJI9vwbYiLLvWDe4OupltRqesXrc";
-const bot = new Telegraf(token);
+bot.help((ctx) => ctx.reply ('трахать мишку чирозиди'));
+bot.command('CumCum', (ctx) => ctx.telegram.leaveChat(ctx.message.chat.id));
+bot.command('ban', (ctx) => ctx.reply ('Гачи топ, зачем банить?'));
+bot.command('leave', (ctx) => ctx.reply ('Я не уйду, пока мне мой Master не прикажет!'));
+bot.command('exit', (ctx) => ctx.reply ('И не надейся)))'));
+bot.command('stop', (ctx) => ctx.reply ('Gachi поезд не остановить!!!'));
 
-/*bot.on("sticker", (ctx) => {
-  const idStick = ctx.update.message.sticker.file_id;
-  add(idStick, StickPath);
-  ctx.reply("Succesfully aded to file");
-});
-bot.on("animation", (ctx) => {
-  const idGif = ctx.update.message.animation.file_id;
-  add(idGif, GifPath);
-  ctx.reply("Succesfully aded to file");
-});*/
-bot.help((ctx) => ctx.reply("трахать мишку Чирозиди"));
-bot.command("CumCum", (ctx) => ctx.telegram.leaveChat(ctx.message.chat.id));
-bot.command("ban", (ctx) => ctx.reply("Гачи топ, зачем банить?"));
-bot.command("leave", (ctx) => ctx.reply("Я не уйду, пока мне мой Master  не прикажет!"));
-bot.command("exit", (ctx) => ctx.reply("И не надейся)))"));
-bot.command("stop", (ctx) => ctx.reply("Gachi поезд не остановить!!!"));
+const spam = (content, n) => {
+  const array = AddToArray(content);
+  const interval = setInterval(() => {
+    if (n <= 0) clearInterval(interval);
+    n--;
+    const rand = randomMessageFromArray(array);
+    if (!!rand) {
+      ctx.reply(rand);
+    }
+  }, 1000);
+};
 
+const randomFromContent = (content) => {
+  const arrayContent = AddToArray(content);
+  const randomElement = randomMessageFromArray(arrayContent);
+  return randomElement;
+};
 
-bot.on("message", async (ctx) => {
+bot.on('message', async (ctx) => {
   const msg = ctx.message;
-  //ctx.reply(msg.message_id);
-  //console.log('msg = ');
-  //console.log(msg);
+
   if (!!msg.text) {
     const text = msg.text.toLowerCase();
-    if (text.includes("бан")) {
-      ctx.reply("Себя забань пидор!");
-    };
-    if (text.includes("ban")) {
-      ctx.reply("Нахуй иди!");
-    };
-    if (text.includes("kik")) {
-      ctx.reply("нихуя не выйдет!");
-    };
-    if (text.includes("кик")) {
-      ctx.reply("Себя блять кикни!");
-    };
 
-    const SpamMes = (contentMes, n) => {
-      const arrayMes = AddToArray(contentMes);
-      const interval = setInterval(() => {
-        if (n <= 0) clearInterval(interval);
-        n--;
-        const messageRand = randomMessageFromArray(arrayMes);
-        if (!!messageRand) {
-          ctx.reply(messageRand);
-        }
-      }, 1000);
-    };
+    const randomGif = (content) => ctx.replyWithAnimation(randomFromContent(content));
+    const randomMess = (content) => ctx.reply(randomFromContent(content));
+    const randomStick = (content) => ctx.replyWithSticker(randomFromContent(content));
 
-    const SpamStick = (contentStick, n) => {
-      const arrayStick = AddToArray(contentStick);
-      const interval = setInterval(() => {
-        if (n <= 0) clearInterval(interval);
-        n--;
-        const stickRand = randomMessageFromArray(arrayStick);
-        if (!!stickRand) {
-          ctx.replyWithSticker(stickRand);
-        }
-      }, 1000);
-    };
-
-    const SpamGif = (contentGif, n) => {
-      const arrayGif = AddToArray(contentGif);
-      const interval = setInterval(() => {
-        if (n <= 0) clearInterval(interval);
-        n--;
-        const gifRand = randomMessageFromArray(arrayGif);
-        if (!!gifRand) {
-          ctx.replyWithAnimation(gifRand);
-        }
-      }, 1000);
-    };
-
-    const randomfromContent = (content) => {
-      const arrayContent = AddToArray(content);
-      const randomElement = randomMessageFromArray(arrayContent);
-      return randomElement;
-    };
-
-    const randomGif =(content) => ctx.replyWithAnimation(randomfromContent(content));
-    const randomMess =(content) => ctx.reply(randomfromContent(content));
-    const randomStick =(content) => ctx.replyWithSticker(randomfromContent(content));
-
-
-    if (text.includes("spammess")) {
-      SpamMes(MessContent, 10);
+    if (text.includes('бан')) {
+      ctx.reply('Себя забань пидор!');
     }
-    if (text.includes("spamstick")) {
-      SpamStick(StickContent, 10);
+    if (text.includes('ban')) {
+      ctx.reply('Нахуй иди!');
     }
-    if (text.includes("spamgif")) {
-      SpamGif(GifContent, 10);
-    };
-    if(text === 'randmes'){
+    if (text.includes('kik')) {
+      ctx.reply('нихуя не выйдет!');
+    }
+    if (text.includes('кик')) {
+      ctx.reply('Себя блять кикни!');
+    }
+    if (text.includes('spammess')) {
+      spam(MessContent, 10);
+    }
+    if (text.includes('spamstick')) {
+      spam(StickContent, 10);
+    }
+    if (text.includes('spamgif')) {
+      spam(GifContent, 10);
+    }
+    if (text.includes('randmes')) {
       randomMess(MessContent);
-    };
-    if(text === 'randgif'){
+    }
+    if (text.includes('randgif')) {
       randomGif(GifContent);
-    };
-    if(text === 'randstick'){
+    }
+    if (text.includes('randstick')) {
       randomStick(StickContent);
-    };
+    }
   }
 });
 
 bot.launch();
-console.log("Похуярили!!");
+console.log('Похуярили!!');
